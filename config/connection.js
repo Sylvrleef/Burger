23267f611fb8ab48bd10b797be7baca5d522e5e7
Sylvrleef <sylvrleef@gmail.com>
2018-01-23 18:08:55 -0600

    added log for connection

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSBD_URL) {
+  console.log("here");
   connection = mysql.createConnection(process.env.JAWSBD_URL);
 } else {
   connection = mysql.createConnection({
