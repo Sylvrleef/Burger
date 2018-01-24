677dbff95208b87a3ece180660bd116eee8c9244
Sylvrleef <sylvrleef@gmail.com>
2018-01-23 18:30:32 -0600

    help

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,18 @@
 var mysql = require("mysql");
 var connection;
 
-// if (process.env.JAWSBD_URL) {
-//   console.log("here");
-//   connection = mysql.createConnection(process.env.JAWSBD_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     port: 3306,
-//     host: "localhost",
-//     user: "root",
-//     password: "^yours001",
-//     database: "burgers_db"
-//   });
-
-connection = mysql.createConnection({
+if (process.env.JAWSBD_URL) {
+  console.log("here");
+  connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+  connection = mysql.createConnection({
     port: 3306,
-    host: "	cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "fbngmuddgg5pan0h	",
-    password: "v0j81aky2elgiu7s",
-    database: "bhwvei1cy7p85ykx"
+    host: "localhost",
+    user: "root",
+    password: "^yours001",
+    database: "burgers_db"
   });
+
   connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
@@ -27,6 +20,6 @@ connection = mysql.createConnection({
     }
     console.log("connected as id " + connection.threadId);
   });
-// };
+};
 
 module.exports = connection;
